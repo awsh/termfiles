2b2b1e948b36b5fa8fe49c4d13b85bbcc8ed2c27
Alan Shiflett <alan.shiflett@gmail.com>
2014-10-07 08:43:10 -0400

    added file cleanup routine

--- tools.py
+++ tools.py
@@ -1,10 +1,32 @@
 import os
 import time
-
+import config
+import errno
+import colorama
 
 def expire_files():
-    # need to recursively travel upload dir and compare file time to current time
+    colorama.init()
+    green = colorama.Fore.GREEN
+    yellow = colorama.Fore.YELLOW
+    red = colorama.Fore.RED
+    reset = colorama.Fore.RESET
 
-    file_time = os.path.getmtime(file) 
-    file_time_plus_24_hours = file_time * config.EXPIRE_TIME * 60 * 60
-    current_time = time.time()
+    print(green + " -- FILE CLEANUP STARTED -- " + reset)
+    for root, dirs, files in os.walk(config.UPLOAD_DIR):
+        for name in files:
+            file_time = os.path.getmtime(os.path.join(root, name)) 
+            expire_time = file_time + config.EXPIRE_TIME * 60 * 60 
+            current_time = time.time()
+            if current_time > expire_time:
+                os.remove(os.path.join(root, name))
+                print(red + "removed: " + os.path.join(root, name) + reset)
+        for name in dirs:
+            try:
+                os.rmdir(os.path.join(root, name))
+                print(red + "Removed: " + os.path.join(root, name) + reset)       
+            except OSError as e:
+                if e.errno == errno.ENOTEMPTY:
+                    print(yellow + os.path.join(root, name) + " is not empty" + reset)
+    print(green + " -- FILE CLEANUP COMPLETED -- " + reset)
+
+ 
