610b44881daf7a68652dd34c667f49993e5723b0
Alan Shiflett <alan.shiflett@gmail.com>
2014-10-07 13:57:24 -0400

    added index page

--- handlers/index.py
+++ handlers/index.py
@@ -3,4 +3,41 @@ from handlers.base import Base
   
 class Index(Base):
     def get(self):
-        self.write('index')
+        contents = """
+       
+        termfiles(1)                    User Manuals                    termfiles(1)
+
+        NAME
+            termfiles.com - terminal based file sharing
+
+        SYNOPSIS
+            curl --upload-file [filename] http://termfiles.com/[filename]
+
+        DESCRIPTION
+            Share files directly from your terminal using any application that supports HTTP PUT
+
+        EXAMPLES
+            To upload a file:
+
+                curl --upload-file test.txt http://termfiles.com/test.txt
+
+            
+            To retrieve a file:
+
+                wget http://termfiles.com/ABCDEFGH/test.txt
+
+            
+            To retrieve multiple files as an archive:
+
+                wget http://termfiles.com/ABCDEFGH/test1.txt+ZYXWVUTS/test2.txt=myfile.zip
+            
+            Supported archive types are zip, tar, tar.gz, tar.bz2
+        
+
+        BUGS
+            Please report any issues at https://github.com/awsh/termfiles/issues
+
+        SEE ALSO
+            http://transfer.sh\n\r\n""" 
+
+        self.write(contents)
